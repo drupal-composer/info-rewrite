de769053860a5cd78e72e6935018d8e2a16b861a
Liam Morland <lkmorlan@uwaterloo.ca>
2019-03-05 10:37:36 -0500

    Create configuration variable drupal-info-rewrite--additional-packageTypes
    Allow configuring additional $packageTypes.

--- src/DrupalInfo.php
+++ src/DrupalInfo.php
@@ -51,6 +51,11 @@ class DrupalInfo implements PluginInterface, EventSubscriberInterface
     {
         $this->composer = $composer;
         $this->io = $io;
+
+        $additionalPackageTypes = $this->composer->getConfig()->get('drupal-info-rewrite--additional-packageTypes');
+        if (is_array($additionalPackageTypes)) {
+            $this->packageTypes = array_merge($this->packageTypes, $additionalPackageTypes);
+        }
     }
 
     /**
